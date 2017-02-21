cfddc1e134a38ad3969d9cc45ebfe6bded6d2b32
ShinuziKyura <up201503716@fe.up.pt>
2017-02-21 18:40:03 +0000

    Version v2.0.1-Beta
    Correction in version numbering
    Not compilable

--- stp.h
+++ stp.h
@@ -1,6 +1,7 @@
 #ifndef SIMPLE_THREAD_POOLS_H_
 #define SIMPLE_THREAD_POOLS_H_
 
+#include <cstdint>
 #include <thread>
 #include <mutex>
 #include <condition_variable>
@@ -56,36 +57,34 @@ namespace stp
 		template <typename ReturnType>
 		void new_task(task<ReturnType> & task)
 		{
-			std::lock_guard<std::mutex> lock_(thread_lock_);
-			task_queue_.push(&task.task_);
-			notify_one(notification_(message::new_task, this));
+			lock_ lock_(thread_lock_);
+			task_queue_.push(std::make_pair(&task.task_, false));
+			lock_.unlock();
+			notify_one(notification_(message_t::new_task, this));
 		}
 		template <typename ReturnType>
 		void new_sync_task(task<ReturnType> & task)
 		{
-			std::lock_guard<std::mutex> lock_(thread_lock_);
-			task_queue_.push(&task.task_);
-			notify_one(notification_(message::new_sync_task, this));
+			lock_ lock_(thread_lock_);
+			task_queue_.push(std::make_pair(&task.task_, true));
+			lock_.unlock();
+			notify_one(notification_(message_t::new_sync_task, this));
 		}
 		void run()
 		{
-			std::lock_guard<std::mutex> lock_(thread_lock_);
-			notify_all(notification_(message::run, this));
+			notify_all(notification_(message_t::run, this));
 		}
 		void run_sync()
 		{
-			std::lock_guard<std::mutex> lock_(thread_lock_);
-			notify_all(notification_(message::run_sync, this));
+			notify_all(notification_(message_t::run_sync, this));
 		}
 		void stop()
 		{
-			std::lock_guard<std::mutex> lock_(thread_lock_);
-			notify_all(notification_(message::stop, this));
+			notify_all(notification_(message_t::stop, this));
 		}
 		void finalize()
 		{
-			std::lock_guard<std::mutex> lock_(thread_lock_);
-			notify_all(notification_(message::finalize, this));
+			notify_all(notification_(message_t::finalize, this));
 		}
 		bool is_running()
 		{
@@ -94,11 +93,11 @@ namespace stp
 		}
 
 		threadpool() = delete;
-		threadpool(size_t threadpool_size) : 
-			threadpool_size_(threadpool_size == 0 ? threadpool_size = std::thread::hardware_concurrency() : threadpool_size)
+		threadpool(uint16_t thread_count) :
+			thread_count_(thread_count == 0 ? std::thread::hardware_concurrency() : thread_count)
 		{
-			thread_array_ = new std::thread[threadpool_size_];
-			for (size_t i = 0; i < threadpool_size_; ++i)
+			thread_array_ = new std::thread[thread_count_];
+			for (size_t i = 0; i < thread_count_; ++i)
 				thread_array_[i] = std::thread(&threadpool::threadpool_, this);
 		}
 		threadpool(threadpool const &) = delete;
@@ -107,19 +106,22 @@ namespace stp
 		threadpool & operator=(threadpool &&) = delete;
 		~threadpool()
 		{
-			std::lock_guard<std::mutex> lock_(thread_lock_);
-			notify_all(notification_(message::terminate, this));
+			notify_all(notification_(message_t::terminate, this));
 
-			for (size_t i = 0; i < threadpool_size_; ++i)
+			for (size_t i = 0; i < thread_count_; ++i)
 				thread_array_[i].join();
 			delete[] thread_array_;
-
-			while (!notification_queue_.empty())
-				delete notification_queue_.front();
 		}
 	private:
-		typedef std::function<void()> task_;
-		enum class message
+		typedef std::pair<std::function<void()> *, bool> task_;
+		typedef std::unique_lock <std::mutex> lock_;
+		enum class state_t
+		{
+			running,
+			waiting,
+			finalizing
+		};
+		enum class message_t
 		{
 			run,
 			run_sync,
@@ -131,34 +133,134 @@ namespace stp
 		};
 		struct notification_
 		{
-			int id;
-			int notified;
-			message msg;
-			notification_(message msg, threadpool * this_) : id(++(this_->notification_id_)), notified(this_->threadpool_size_), msg(msg)
+			uint64_t id;
+			uint16_t notified;
+			message_t msg;
+			notification_(message_t msg, threadpool * this_) : id(++(this_->notification_id_)), notified(this_->thread_count_), msg(msg)
 			{
+				if (id == 0)
+					id = ++(this_->notification_id_);
 			}
 		};
 
-		std::queue<task_ *> task_queue_;
-		std::queue<notification_ *> notification_queue_;
+		std::queue<task_> task_queue_;
+		std::queue<notification_> notification_queue_;
 		std::thread * thread_array_;
 		std::mutex thread_lock_;
 		std::condition_variable thread_alert_;
-		size_t const threadpool_size_;
-		size_t notification_id_ = 0;
+		uint16_t const thread_count_;
+		uint64_t notification_id_ = 0;
 
 		void notify_one(notification_ notification_)
 		{
-			// To do
+			lock_ lock_(thread_lock_);
+			notification_queue_.push(notification_);
+			lock_.unlock();
+			thread_alert_.notify_one();
 		}
 		void notify_all(notification_ notification_)
 		{
-			// To do
+			lock_ lock_(thread_lock_);
+			notification_queue_.push(notification_);
+			lock_.unlock();
+			thread_alert_.notify_all();
 		}
 		void threadpool_()
 		{
-			// To do
-			while (true);
+			uint64_t last_id_ = 0;
+			message_t last_msg_;
+			state_t state_ = state_t::waiting;
+			task_ task_(nullptr, false);
+			lock_ lock_(thread_lock_);
+
+			do
+			{
+				thread_alert_.wait(lock_, [=]
+				{
+					return (!notification_queue_.empty() && notification_queue_.front().id != last_id_);
+				});
+				last_id_ = notification_queue_.front().id;
+				last_msg_ = notification_queue_.front().msg;
+				if (--notification_queue_.front().notified == 0)
+				{
+					notification_queue_.pop();
+				}
+
+				switch (last_msg_)
+				{
+					case message_t::run:
+						running = true;
+						if (task_.first)
+						{
+							if (!task_.second)
+							{
+								lock_.unlock();
+								(*task_.first)();
+								lock_.lock();
+							}
+						}
+						else
+						{
+							if (!task_queue_.empty())
+							{
+								task_ = task_queue_.front();
+								task_queue_.pop();
+								if (!task_.second)
+								{
+									lock_.unlock();
+									(*task_.first)();
+									task_.first = nullptr;
+									lock_.lock();
+								}
+							}
+						}
+						continue;
+					case message_t::run_sync:
+						if (running)
+						{
+							if (task_.first && task_.second)
+							{
+								lock_.unlock();
+								(*task_.first)();
+								task_.first = nullptr;
+								lock_.lock();
+							}
+						}
+						continue;
+					case message_t::new_task:
+						if (running)
+						{
+							if (!task_queue_.empty())
+							{
+								task_ = task_queue_.front();
+								task_queue_.pop();
+								lock_.unlock();
+								(*task_.first)();
+								task_.first = nullptr;
+								lock_.lock();
+							}
+						}
+						continue;
+					case message_t::new_sync_task:
+						if (running)
+						{
+							if (!task_queue_.empty())
+							{
+								task_ = task_queue_.front();
+								task_queue_.pop();
+							}
+						}
+						continue;
+					case message_t::stop:
+						running = false;
+						continue;
+					case message_t::finalize:
+						continue;
+					case message_t::terminate:
+						continue;
+				}
+			}
+			while (false);
 		}
 	};
 }
