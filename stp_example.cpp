ff89dbdb8baefa63b38ee97586c18a204640ddf2
ShinuziKyura <up201503716@fe.up.pt>
2017-03-12 01:07:33 +0000

    Version 3.0.1-Beta

--- stp_example.cpp
+++ stp_example.cpp
@@ -10,25 +10,27 @@ std::random_device seed;
 std::mt19937 generate(seed());
 std::uniform_int_distribution<int> random_list(std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
 
-std::chrono::duration<double, std::nano> generator(std::vector<int> * vec)
+std::chrono::duration<double, std::nano> generator(std::vector<int> & vec)
 {
-	auto start_timer = std::chrono::high_resolution_clock::now();
-	for (auto & i : *vec)
+	std::chrono::time_point<std::chrono::high_resolution_clock> start_timer, stop_timer;
+	start_timer = std::chrono::high_resolution_clock::now();
+	for (auto & i : vec)
 	{
 		i = random_list(generate);
 	}
-	auto stop_timer = std::chrono::high_resolution_clock::now();
+	stop_timer = std::chrono::high_resolution_clock::now();
 	return std::chrono::duration<double, std::nano>(stop_timer - start_timer);
 }
 
-std::chrono::duration<double, std::nano> quicksort(std::vector<int> * vec)
+std::chrono::duration<double, std::nano> quicksort(std::vector<int> & vec)
 {
-	auto start_timer = std::chrono::high_resolution_clock::now();
-	std::sort(vec->begin(), vec->end(), [] (int const & i1, int const & i2) -> bool
+	std::chrono::time_point<std::chrono::high_resolution_clock> start_timer, stop_timer;
+	start_timer = std::chrono::high_resolution_clock::now();
+	std::sort(vec.begin(), vec.end(), [] (int const & i1, int const & i2) -> bool
 	{
 		return i1 < i2;
 	});
-	auto stop_timer = std::chrono::high_resolution_clock::now();
+	stop_timer = std::chrono::high_resolution_clock::now();
 	return std::chrono::duration<double, std::nano>(stop_timer - start_timer);
 }
 
@@ -38,42 +40,42 @@ int main()
 
 	std::cout << "Generating vector 1..." << std::endl;
 	std::vector<int> vec_1(1000000);
-	auto elapsed1 = generator(&vec_1);
+	auto elapsed1 = generator(vec_1);
 	std::cout << "Elapsed: " << elapsed1.count() << "ns" << std::endl;
 
 	std::cout << "Generating vector 2..." << std::endl;
 	std::vector<int> vec_2(1000000);
-	auto elapsed2 = generator(&vec_2);
+	auto elapsed2 = generator(vec_2);
 	std::cout << "Elapsed: " << elapsed2.count() << "ns" << std::endl;
 
 	std::cout << "Generating vector 3..." << std::endl;
 	std::vector<int> vec_3(1000000);
-	auto elapsed3 = generator(&vec_3);
+	auto elapsed3 = generator(vec_3);
 	std::cout << "Elapsed: " << elapsed3.count() << "ns" << std::endl;
 
 	std::cout << "Generating vector 4..." << std::endl;
 	std::vector<int> vec_4(1000000);
-	auto elapsed4 = generator(&vec_4);
+	auto elapsed4 = generator(vec_4);
 	std::cout << "Elapsed: " << elapsed4.count() << "ns" << std::endl;
 
 	std::cout << "Generating vector 5..." << std::endl;
 	std::vector<int> vec_5(1000000);
-	auto elapsed5 = generator(&vec_5);
+	auto elapsed5 = generator(vec_5);
 	std::cout << "Elapsed: " << elapsed5.count() << "ns" << std::endl;
 
 	std::cout << "Generating vector 6..." << std::endl;
 	std::vector<int> vec_6(1000000);
-	auto elapsed6 = generator(&vec_6);
+	auto elapsed6 = generator(vec_6);
 	std::cout << "Elapsed: " << elapsed6.count() << "ns" << std::endl;
 
 	std::cout << "Generating vector 7..." << std::endl;
 	std::vector<int> vec_7(1000000);
-	auto elapsed7 = generator(&vec_7);
+	auto elapsed7 = generator(vec_7);
 	std::cout << "Elapsed: " << elapsed7.count() << "ns" << std::endl;
 
 	std::cout << "Generating vector 8..." << std::endl;
 	std::vector<int> vec_8(1000000);
-	auto elapsed8 = generator(&vec_8);
+	auto elapsed8 = generator(vec_8);
 	std::cout << "Elapsed: " << elapsed8.count() << "ns" << std::endl;
 
 	// Sorting first four vectors (without concurrency)
@@ -82,19 +84,19 @@ int main()
 	auto start_timer = std::chrono::high_resolution_clock::now();
 
 	std::cout << "Sorting vector 1..." << std::endl;
-	elapsed1 = quicksort(&vec_1);
+	elapsed1 = quicksort(vec_1);
 	std::cout << "Elapsed: " << elapsed1.count() << "ns" << std::endl;
 
 	std::cout << "Sorting vector 2..." << std::endl;
-	elapsed2 = quicksort(&vec_2);
+	elapsed2 = quicksort(vec_2);
 	std::cout << "Elapsed: " << elapsed2.count() << "ns" << std::endl;
 
 	std::cout << "Sorting vector 3..." << std::endl;
-	elapsed3 = quicksort(&vec_3);
+	elapsed3 = quicksort(vec_3);
 	std::cout << "Elapsed: " << elapsed3.count() << "ns" << std::endl;
 
 	std::cout << "Sorting vector 4..." << std::endl;
-	elapsed4 = quicksort(&vec_4);
+	elapsed4 = quicksort(vec_4);
 	std::cout << "Elapsed: " << elapsed4.count() << "ns" << std::endl;
 
 	auto stop_timer = std::chrono::high_resolution_clock::now();
@@ -103,10 +105,10 @@ int main()
 	// Sorting second four vectors (with concurrency)
 
 	stp::threadpool threadpool(thread_amount);
-	stp::task<std::chrono::duration<double, std::nano>> task_1(quicksort, &vec_5);
-	stp::task<std::chrono::duration<double, std::nano>> task_2(quicksort, &vec_6);
-	stp::task<std::chrono::duration<double, std::nano>> task_3(quicksort, &vec_7);
-	stp::task<std::chrono::duration<double, std::nano>> task_4(quicksort, &vec_8);
+	stp::task<std::chrono::duration<double, std::nano>> task_1(quicksort, std::ref(vec_5));
+	stp::task<std::chrono::duration<double, std::nano>> task_2(quicksort, std::ref(vec_6));
+	stp::task<std::chrono::duration<double, std::nano>> task_3(quicksort, std::ref(vec_7));
+	stp::task<std::chrono::duration<double, std::nano>> task_4(quicksort, std::ref(vec_8));
 	
 	threadpool.run();
 	threadpool.new_sync_task(task_1);
