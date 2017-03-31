6a6bc84d142685ae4035eb469c34a64b626ee46f
ShinuziKyura <up201503716@fe.up.pt>
2017-03-31 05:35:05 +0100

    Version 3.2.1-Beta

--- stp_example.cpp
+++ stp_example.cpp
@@ -34,10 +34,10 @@ int main()
 	constexpr size_t thread_single = 1;
 	constexpr size_t thread_amount = 4; // 0 == std::thread::hardware_concurrency()
 
-	// Generating vectors
+	std::cout.precision(5);
+	std::cout << std::scientific;
 
-	std::cout.precision(6);
-	std::cout << "Generating eight vectors..." << std::endl;
+	// Generating vectors	
 
 	std::vector<int> vec_1(1000000);
 	std::vector<int> vec_2(1000000);
@@ -47,18 +47,37 @@ int main()
 	std::vector<int> vec_6(1000000);
 	std::vector<int> vec_7(1000000);
 	std::vector<int> vec_8(1000000);
+	stp::task<double> task_1(generator, std::ref(vec_1));
+	stp::task<double> task_2(generator, std::ref(vec_2));
+	stp::task<double> task_3(generator, std::ref(vec_3));
+	stp::task<double> task_4(generator, std::ref(vec_4));
+	stp::task<double> task_5(generator, std::ref(vec_5));
+	stp::task<double> task_6(generator, std::ref(vec_6));
+	stp::task<double> task_7(generator, std::ref(vec_7));
+	stp::task<double> task_8(generator, std::ref(vec_8));
 
-	std::cout << "Elapsed on vector 1: " << generator(vec_1) << "ns" << std::endl;
-	std::cout << "Elapsed on vector 2: " << generator(vec_2) << "ns" << std::endl;
-	std::cout << "Elapsed on vector 3: " << generator(vec_3) << "ns" << std::endl;
-	std::cout << "Elapsed on vector 4: " << generator(vec_4) << "ns" << std::endl;
-	std::cout << "Elapsed on vector 5: " << generator(vec_5) << "ns" << std::endl;
-	std::cout << "Elapsed on vector 6: " << generator(vec_6) << "ns" << std::endl;
-	std::cout << "Elapsed on vector 7: " << generator(vec_7) << "ns" << std::endl;
-	std::cout << "Elapsed on vector 8: " << generator(vec_8) << "ns" << std::endl;
+	std::cout << "Generating eight vectors..." << std::endl;
+
+	task_1();
+	task_2();
+	task_3();
+	task_4();
+	task_5();
+	task_6();
+	task_7();
+	task_8();
+
+	std::cout << "Elapsed on vector 1: " << task_1.result() << "ns" << std::endl;
+	std::cout << "Elapsed on vector 2: " << task_2.result() << "ns" << std::endl;
+	std::cout << "Elapsed on vector 3: " << task_3.result() << "ns" << std::endl;
+	std::cout << "Elapsed on vector 4: " << task_4.result() << "ns" << std::endl;
+	std::cout << "Elapsed on vector 5: " << task_5.result() << "ns" << std::endl;
+	std::cout << "Elapsed on vector 6: " << task_6.result() << "ns" << std::endl;
+	std::cout << "Elapsed on vector 7: " << task_7.result() << "ns" << std::endl;
+	std::cout << "Elapsed on vector 8: " << task_8.result() << "ns" << std::endl;
 
 	// Sorting first four vectors (without concurrency)
-	
+
 	{
 		stp::threadpool threadpool_1(thread_single);
 		stp::task<double> task_1(sorter, std::ref(vec_1));
@@ -71,7 +90,7 @@ int main()
 		threadpool_1.new_task(task_3);
 		threadpool_1.new_task(task_4);
 
-		std::cout << "Sorting first four vectors...\n";
+		std::cout << "Sorting first four vectors...\n[Without concurrency]\n";
 
 		start_timer = std::chrono::high_resolution_clock::now();
 
@@ -88,9 +107,10 @@ int main()
 		std::cout << "Elapsed on vector 2: " << task_2.result() << "ns" << std::endl;
 		std::cout << "Elapsed on vector 3: " << task_3.result() << "ns" << std::endl;
 		std::cout << "Elapsed on vector 4: " << task_4.result() << "ns" << std::endl;
+
 		std::cout << "Elapsed on first four vectors: " << std::chrono::duration<double, std::nano>(stop_timer - start_timer).count() << "ns" << std::endl;
 	}
-	
+
 	// Sorting second four vectors (with concurrency)
 
 	{
@@ -110,7 +130,7 @@ int main()
 			std::this_thread::sleep_for(std::chrono::milliseconds(1));
 		}
 
-		std::cout << "Sorting second four vectors...\n";
+		std::cout << "Sorting second four vectors...\n[With concurrency]\n";
 
 		start_timer = std::chrono::high_resolution_clock::now();
 
@@ -127,6 +147,7 @@ int main()
 		std::cout << "Elapsed on vector 6: " << task_6.result() << "ns" << std::endl;
 		std::cout << "Elapsed on vector 7: " << task_7.result() << "ns" << std::endl;
 		std::cout << "Elapsed on vector 8: " << task_8.result() << "ns" << std::endl;
+
 		std::cout << "Elapsed on second four vectors: " << std::chrono::duration<double, std::nano>(stop_timer - start_timer).count() << "ns" << std::endl;
 	}
 
